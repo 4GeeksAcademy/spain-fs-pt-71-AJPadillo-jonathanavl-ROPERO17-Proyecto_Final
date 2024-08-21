9d7a52408a1a4c056994d7cba1e6849abbb2b93b
Antonio Jesús Padillo <98108563+AJPadillo@users.noreply.github.com>
2024-08-21 12:06:12 +0000

    2.8 añadido navbar para manejo de sesion

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+    const { store, actions } = useContext(Context);
+    const navigate = useNavigate();
+
+    // Manejar logout
+    const handleLogout = () => {
+        actions.logout();
+        navigate("/");
+    };
+
+    return (
+        <nav className="navbar navbar-light bg-light mb-3">
+            <div className="container">
+                <Link to="/">
+                    <span className="navbar-brand mb-0 h1"><img src="" style={{ width: "150px" }} />Logo</span>
+                </Link>
+                <div className="ml-auto d-flex">
+                    <div className="m-auto">
+                        {store.isLoggedIn && (
+                            <p className="mb-0 mx-3">Hola, {store.currentUser.email}</p>
+                        )}
+                    </div>
+                    
+                    {/* Botones de inicio de sesión/registro o cerrar sesión según el estado de autenticación */}
+                    {!store.isLoggedIn ? (
+                        <>
+                            <Link to="/login">
+                                <button className="btn btn-outline-primary mx-2">Login</button>
+                            </Link>
+                            <Link to="/signup">
+                                <button className="btn btn-outline-secondary">Sign Up</button>
+                            </Link>
+                        </>
+                    ) : (
+                        <button className="btn btn-outline-danger" onClick={handleLogout}>Logout</button>
+                    )}
+
+                </div>
+
+            </div>
+        </nav>
+    );
 };
